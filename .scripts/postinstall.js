de5bcd2edd189d003d3f2069eae09711b875a9e0
Philipp <github@piwo.space>
2024-04-29 09:57:34 +0200

    chore: upgrade aeproject, expose aci

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
 
 const tokenRegistry = fs.readFileSync(__dirname + '/../contracts/token-registry.aes', 'utf-8');
-fs.writeFileSync(__dirname + '/../TokenRegistry.aes.js', `module.exports = \`\n${tokenRegistry}\`;\n`, 'utf-8');
-
-const tokenRegistryInterface = fs.readFileSync(__dirname + '/../contracts/token-registry-interface.aes', 'utf-8');
-fs.writeFileSync(__dirname + '/../TokenRegistryInterface.aes.js', `module.exports = \`\n${tokenRegistryInterface}\`;\n`, 'utf-8');
+fs.writeFileSync(__dirname + '/../generated/TokenRegistry.aes.js', `module.exports = \`\n${tokenRegistry}\`;\n`, 'utf-8');
